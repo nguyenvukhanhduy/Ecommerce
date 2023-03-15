f6c0167244a93e5ae4549041a98f115a25893b05
Nguyễn Vũ Khánh Huy <huydot828613@MacBook-Air-cua-Nguyen.local>
2023-03-15 15:17:49 +0700

    Custom Dynamic Middleware for ApiKey and Permissions

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,13 @@
 
 const express = require('express')
 const router = express.Router()
+const {apiKey, permission} = require('../auth/checkAuth')
+
+//check apiKey
+router.use(apiKey)
+//check permission
+router.use(permission('0000'))
 
 router.use('/v1/api', require('./access'))
 
-// router.get('', (req, res, next) => {
-//     return res.status(200).json({
-//         message: 'hello',
-//     })
-// })
 module.exports = router
