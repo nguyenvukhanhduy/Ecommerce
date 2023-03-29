9c6af6332e7f6d4f291f4ad763d68795322d12cf
Nguyễn Vũ Khánh Huy <huydot828613@MacBook-Air-cua-Nguyen.local>
2023-03-29 18:18:26 +0700

    Logout and fix issue verify publicToken

--- src/auth/authUtils.js
+++ src/auth/authUtils.js
@@ -1,10 +1,19 @@
 'use strict'
 const JWT = require('jsonwebtoken')
-const { decode } = require('punycode')
+const { AuthFailureError, NotFoundError } = require('../core/error.response')
+const asyncHandler = require('../helpers/asyncHandler')
+const { findByUserId } = require('../services/keyToken.service')
+
+const HEADER = {
+    API_KEY: 'x-api-key',
+    CLIENT_ID: 'x-client-id',
+    AUTHORIZATION: 'authorization'
+}
+
 const createTokenPair = async (payload, publicKey, privateKey) => {
     try {
         //accessToken
-        const accessToken = await JWT.sign(payload, privateKey, {
+        const accessToken = await JWT.sign(payload, publicKey, {
             expiresIn: '2 days'
         })
         //refreshToken
@@ -13,9 +22,9 @@ const createTokenPair = async (payload, publicKey, privateKey) => {
         })
 
         //
-        JWT.verify(accessToken, publicKey, (err, decode)=>{
-            if(err){
-                console.log('error verify::' , err)
+        JWT.verify(accessToken, publicKey, (err, decode) => {
+            if (err) {
+                console.log('error verify::', err)
             } else {
                 console.log('decode verify::', decode)
             }
@@ -25,6 +34,26 @@ const createTokenPair = async (payload, publicKey, privateKey) => {
 
     }
 }
+const authentication = asyncHandler(async (req, res, next) => {
+    const userId = req.headers[HEADER.CLIENT_ID]
+    if (!userId) throw new AuthFailureError('Invalid request')
+
+    const keyStore = await findByUserId(userId)
+    if (!keyStore) throw new NotFoundError('Not Found keyStore')
+
+    const accessToken = req.headers[HEADER.AUTHORIZATION]
+    if (!accessToken) throw new AuthFailureError('Invalid Request')
+
+    try {
+        const decodeUser = JWT.verify(accessToken, keyStore.publicKey)
+        if (userId !== decodeUser.userId) throw new AuthFailureError('Invalid User')
+        req.keyStore = keyStore
+        return next()
+    } catch (error) {
+        throw error
+    }
+})
 module.exports = {
-    createTokenPair
+    createTokenPair,
+    authentication
 }
--- src/services/keyToken.service.js
+++ src/services/keyToken.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const keytokenModel = require("../models/keytoken.model")
-
+const { Types } = require('mongoose')
 class KeyTokenService {
 
     static createKeyToken = async ({ userId, publicKey, privateKey, refreshToken }) => {
@@ -15,9 +15,9 @@ class KeyTokenService {
             // return tokens ? tokens.publicKey : null
 
             //levelxx
-            const filter = {user: userId}, update = {
+            const filter = { user: userId }, update = {
                 publicKey, privateKey, refreshTokensUsed: [], refreshToken
-            }, options = { upsert: true, new: true}
+            }, options = { upsert: true, new: true }
 
             const tokens = await keytokenModel.findOneAndUpdate(filter, update, options)
 
@@ -25,6 +25,14 @@ class KeyTokenService {
         } catch (error) {
             return error
         }
+    }
+
+    static findByUserId = async (userId) => {
+        return await keytokenModel.findOne({ user: new Types.ObjectId(userId) }).lean()
+    }
+
+    static removeKeyById = async (id) => {
+        return await keytokenModel.findByIdAndRemove(id)
     }
 }
 module.exports = KeyTokenService
